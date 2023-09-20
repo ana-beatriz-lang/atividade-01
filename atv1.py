3557deb0ea787cc7e7edddb1d3221b62505f8977
ana-beatriz-lang <145585125+ana-beatriz-lang@users.noreply.github.com>
2023-09-20 12:15:40 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,15 @@
+print("ola mundo")
+num1=float(input("dijite o primeiro numero"))
+num2=float(input("dijite o segundo numero"))
+
+#Realiza operacao
+soma=num1+num2
+subtracao=num1-num2
+multiplicacao=num1*num2
+divisao=num1/num2
+
+#Exibe os resultados
+print(f"soma:{soma}")
+print(f"subtracao:{subtracao}")
+print(f"multiplicacao{multiplicacao}")
+print(f"divisao{divisao}")
